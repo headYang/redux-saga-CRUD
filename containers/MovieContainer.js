6baf2c24e2671c6912ea911cc70a923831cf2ac4
jinyang <yang.youth1020@gmail.com>
2018-07-30 18:06:08 +0200

    Add MovieContainer

---
+++ containers/MovieContainer.js
@@ -0,0 +1,18 @@
+import {} from 'react-redux';
+import MovieComponent from '../components/MovieComponent';
+
+//Action
+const mapStateToProps = (state) => {
+    return {
+
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    }
+};
+
+const MovieContainer = connect(mapStateToProps, mapDispatchToProps)(MovieComponent);
+export default MovieContainer;
